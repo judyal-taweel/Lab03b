67fd769f174a1b5c8fc3cdb6617c2dbb7aa3857a
judyal-taweel <judybataineh@gmail.com>
2021-01-27 15:56:20 +0200

    java scirept

---
+++ Lab04.js
@@ -0,0 +1,41 @@
+
+var yourName = prompt('What is your name?');
+
+alert('hi ' + yourName)
+console.log(yourName);
+
+
+var yourAge = prompt('how old are you?');
+
+
+
+if(yourAge < 50){
+alert('You will go trip to space');
+}else{
+alert('We are invited to watch the best film about space ');
+};
+
+console.log(yourAge);
+
+
+var yourPlanet = prompt('What is the name of your planet?');
+if(yourPlanet == 'earth'){
+    document.write('<h1>'+'Hello in our planet, you are from ' + yourPlanet +'<h1>');
+}else {
+    document.write('<h1>'+'Are you an alien creature? you are from ' + yourPlanet +'<h1>');
+}
+
+
+
+
+var number1 = 10;
+var number2 = "10";
+console.log(number1 == number2);
+
+function myFunction() {
+    document.body.style.backgroundColor = "#ffcccc";
+
+}
+function myFunction2() {
+document.body.style.backgroundColor = "#f0b3ff";
+}
